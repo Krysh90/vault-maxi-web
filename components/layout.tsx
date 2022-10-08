16cd32e532e9e4b14760686365532376389d7c37
Krysh <krysh.developer@gmail.com>
2022-10-08 23:59:52 +0200

    [web] adjusting title

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from './layout.module.css'
 
-export const siteTitle = 'Vault-Maxi'
+export const siteTitle = 'Vault Maxi'
 
 interface LayoutProps {
   children: any
@@ -11,9 +11,10 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <div className={styles.container}>
       <Head>
+        <title>{siteTitle}</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Learn how vault-maxi works and what it can do for you" />
-        <meta name="og:title" content="Vault Maxi" />
+        <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <main className={styles.main}>{children}</main>
