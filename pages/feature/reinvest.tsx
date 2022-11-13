5882eddea70558c0ab7b2d20c6f2dd957a3268cd
Krysh <krysh.developer@gmail.com>
2022-11-13 10:26:12 +0100

    [reinvest] changed reinvest feature description to new powerful reinvest

--- pages/feature/reinvest.tsx
+++ pages/feature/reinvest.tsx
@@ -9,20 +9,60 @@ const Reinvest: NextPage = () => {
     <Layout page="Reinvest">
       <PageHeader pageHeader="Reinvest">
         <Image src={overview} alt="Overview of reinvest" />
-        <h4>
-          <strong>Important note</strong>: We are currently completely overhauling our reinvest. Stay tuned!
-        </h4>
         <div>
           <p>
-            Reinvest is another standalone bot, which can be hosted on your AWS. It a really simple automation, which
-            triggers on having above a configured amount of DFI on your address.
+            Reinvest is now 100% customizable and powerful as it gets. This feature is completely integrated into{' '}
+            <em>Vault-Maxi</em> and can be used as a standalone bot, which can be hosted on your AWS. It a real powerful
+            automation, which triggers on having above a configured amount of DFI on your address.
           </p>
           <p>
-            As soon as this threshold is met, it will swap 50% DFI into the first token of your configured pool and the
-            other 50% DFI into the second token. After the swap is completed, it will add those tokens into the
-            liquidity pool.
+            As soon as this threshold is met, it will swap, send, deposit and/or add to liquidity pool depending on your
+            reinvestment targets, which are described by a pattern.
           </p>
-          <p>All pools which are currently available on DeFiChain can be used.</p>
+          <p>This pattern contains one or more reinvest targets. Each reinvest target has three parts:</p>
+          <ul className="pl-6">
+            <li className="list-disc">
+              target token, either a token like <em>DFI</em>, <em>DUSD</em>... or a liquidity mining token like{' '}
+              <em>TSLA-DUSD</em>...
+            </li>
+            <li className="list-disc">percentage which should be used for this target</li>
+            <li className="list-disc">address or vault, where this should be sent or deposited</li>
+          </ul>
+          <h4 className="pt-2">Example patterns</h4>
+          <p className="text-code font-mono">DFI TSLA-DUSD</p>
+          <p>will lead to:</p>
+          <ul className="pl-6">
+            <li className="list-disc text-code font-mono">DFI</li>
+            <li>
+              will deposit 50% of your reinvested amount as <em>DFI</em> to your configured vault
+            </li>
+            <li className="list-disc text-code font-mono">TSLA-DUSD</li>
+            <li>
+              will swap & add 50% of your reinvested amount in <em>TSLA-DUSD</em> pool
+            </li>
+          </ul>
+          <p className="pt-2 text-code font-mono">DFI:40 BTC-DFI:30:addressOfJoe DUSD::vaultOfJane</p>
+          <p>will lead to:</p>
+          <ul className="pl-6">
+            <li className="list-disc text-code font-mono">DFI:40</li>
+            <li>
+              will deposit 40% of your reinvested amount as <em>DFI</em> to your configured vault
+            </li>
+            <li className="list-disc text-code font-mono">BTC-DFI:30:addressOfSomeOneElse</li>
+            <li>
+              will swap & add 30% of your reinvested amount in <em>BTC-DFI</em> pool and send it to the address of Joe
+            </li>
+            <li className="list-disc text-code font-mono">DUSD::vaultOfJane</li>
+            <li>
+              {' '}
+              will swap 40% of your reinvested amount in <em>DUSD</em> pool and deposit it to the vault of Jane
+            </li>
+          </ul>
+          <p className="pt-4">
+            <strong className="text-main">Important:</strong> We are currently developing a tool to create such
+            patterns. If it sounds too complicated to write it yourself, please wait until we launched this tool. As
+            soon as we are finished a link to this tool will be here.
+          </p>
         </div>
       </PageHeader>
     </Layout>
