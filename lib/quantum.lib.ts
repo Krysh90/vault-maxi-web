21d5ed9b2a8e6af3fcdfe783df7e44c8789314eb
Krysh <krysh.developer@gmail.com>
2023-04-17 21:51:40 +0200

    [quantum] added slider for data selection

--- lib/quantum.lib.ts
+++ lib/quantum.lib.ts
@@ -11,8 +11,13 @@ export enum QuantumChartDataType {
   VOLUME_DFC = 'VOLUME DFC',
   LIQUIDITY_ETH = 'LIQUIDITY ETH',
   LIQUIDITY_DFC = 'LIQUIDITY DFC',
-  TOTAL_VOLUME_ETH = 'TOTAL VOLUME ETH',
-  TOTAL_VOLUME_DFC = 'TOTAL VOLUME DFC',
+  QUANTUM_LIQUIDITY = 'QUANTUM LIQUIDITY',
+  QUANTUM_VOLUME_ETH = 'QUANTUM VOLUME ETH',
+  QUANTUM_VOLUME_DFC = 'QUANTUM VOLUME DFC',
+  QUANTUM_LIQUIDITY_ETH = 'QUANTUM LIQUIDITY ETH',
+  QUANTUM_LIQUIDITY_DFC = 'QUANTUM LIQUIDITY DFC',
+  QUANTUM_TOTAL_VOLUME_ETH = 'QUANTUM TOTAL VOLUME ETH',
+  QUANTUM_TOTAL_VOLUME_DFC = 'QUANTUM TOTAL VOLUME DFC',
 }
 
 export function historyDaysToLoad(): string[] {
@@ -85,6 +90,54 @@ export function toChartData(stats: QuantumStats, { type, sort }: ChartInfo): Cha
           color: colorBasedOn(entry.tokenName),
         }))
       break
+    case QuantumChartDataType.QUANTUM_LIQUIDITY:
+      const quantumDefichain = Object.entries(stats.quantumData.liqDfc)
+        .map(([token, amount]) => ({ token, amount }))
+        .sort((a, b) => a.token.localeCompare(b.token))
+      const quantumEthereum = Object.entries(stats.quantumData.liqEth)
+        .map(([token, amount]) => ({ token, amount }))
+        .sort((a, b) => b.token.localeCompare(a.token))
+      entries = quantumDefichain.map((entry) => ({
+        label: entry.token + ' on defichain',
+        data: new BigNumber(entry.amount).multipliedBy(getPriceOf(entry.token, stats)).toNumber(),
+        color: colorBasedOn(entry.token),
+      }))
+      entries.push({
+        label: '',
+        data: totalAmountOfLiquidity(quantumDefichain, stats)
+          .minus(totalAmountOfLiquidity(quantumEthereum, stats))
+          .absoluteValue()
+          .toNumber(),
+        color: '#111',
+      })
+      entries = entries.concat(
+        ...quantumEthereum.map((entry) => ({
+          label: entry.token + ' on Ethereum',
+          data: new BigNumber(entry.amount).multipliedBy(getPriceOf(entry.token, stats)).toNumber(),
+          color: colorBasedOn(entry.token),
+        })),
+      )
+      break
+    case QuantumChartDataType.QUANTUM_VOLUME_ETH:
+      entries = Object.entries(stats.quantumData.txsToEth.amountBridged)
+        .map(([token, amount]) => ({ token, amount }))
+        .filter((entry) => new BigNumber(entry.amount).isGreaterThan(0))
+        .map((entry) => ({
+          label: entry.token,
+          data: new BigNumber(entry.amount).multipliedBy(getPriceOf(entry.token, stats)).toNumber(),
+          color: colorBasedOn(entry.token),
+        }))
+      break
+    case QuantumChartDataType.QUANTUM_VOLUME_DFC:
+      entries = Object.entries(stats.quantumData.txsToDfc.amountBridged)
+        .map(([token, amount]) => ({ token, amount }))
+        .filter((entry) => new BigNumber(entry.amount).isGreaterThan(0))
+        .map((entry) => ({
+          label: entry.token,
+          data: new BigNumber(entry.amount).multipliedBy(getPriceOf(entry.token, stats)).toNumber(),
+          color: colorBasedOn(entry.token),
+        }))
+      break
   }
   if (sort) entries = entries.sort((a, b) => b.data - a.data)
   return {
@@ -103,6 +156,8 @@ export function toLineChartData(history: QuantumStats[], { type, timeFrame }: Li
   const entries: LineChartEntry[] = []
   const defichainTokens = Object.keys(history.slice(-1)[0].liquidity.defichain)
   const ethereumTokens = Object.keys(history.slice(-1)[0].liquidity.ethereum)
+  const quantumDfcTokens = Object.keys(history.slice(-1)[0].quantumData.liqDfc)
+  const quantumEthTokens = Object.keys(history.slice(-1)[0].quantumData.liqEth)
   switch (type) {
     case QuantumChartDataType.LIQUIDITY_DFC:
       entries.push(
@@ -160,9 +215,73 @@ export function toLineChartData(history: QuantumStats[], { type, timeFrame }: Li
         })),
       )
       break
-    case QuantumChartDataType.TOTAL_VOLUME_DFC:
+    case QuantumChartDataType.QUANTUM_LIQUIDITY_DFC:
       entries.push(
-        ...ethereumTokens.map((token) => ({
+        ...quantumDfcTokens.map((token) => ({
+          label: token,
+          data: history
+            .filter((entry) => entry.quantumData.liqDfc !== undefined && entry.quantumData.liqDfc[token])
+            .map((entry) =>
+              new BigNumber(entry.quantumData.liqDfc[token]).multipliedBy(getPriceOf(token, entry)).toNumber(),
+            ),
+          color: colorBasedOn(token),
+        })),
+      )
+      break
+    case QuantumChartDataType.QUANTUM_LIQUIDITY_ETH:
+      entries.push(
+        ...quantumEthTokens.map((token) => ({
+          label: token,
+          data: history
+            .filter((entry) => entry.quantumData.liqEth !== undefined && entry.quantumData.liqEth[token])
+            .map((entry) =>
+              new BigNumber(entry.quantumData.liqEth[token]).multipliedBy(getPriceOf(token, entry)).toNumber(),
+            ),
+          color: colorBasedOn(token),
+        })),
+      )
+      break
+    case QuantumChartDataType.QUANTUM_VOLUME_DFC:
+      entries.push(
+        ...quantumDfcTokens.map((token) => ({
+          label: token,
+          data: history
+            .filter(
+              (entry) =>
+                entry.quantumData.txsToDfc.amountBridged !== undefined &&
+                entry.quantumData.txsToDfc.amountBridged[token],
+            )
+            .map((entry) =>
+              new BigNumber(entry.quantumData.txsToDfc.amountBridged[token])
+                .multipliedBy(getPriceOf(token, entry))
+                .toNumber(),
+            ),
+          color: colorBasedOn(token),
+        })),
+      )
+      break
+    case QuantumChartDataType.QUANTUM_VOLUME_ETH:
+      entries.push(
+        ...quantumEthTokens.map((token) => ({
+          label: token,
+          data: history
+            .filter(
+              (entry) =>
+                entry.quantumData.txsToEth.amountBridged !== undefined &&
+                entry.quantumData.txsToEth.amountBridged[token],
+            )
+            .map((entry) =>
+              new BigNumber(entry.quantumData.txsToEth.amountBridged[token])
+                .multipliedBy(getPriceOf(token, entry))
+                .toNumber(),
+            ),
+          color: colorBasedOn(token),
+        })),
+      )
+      break
+    case QuantumChartDataType.QUANTUM_TOTAL_VOLUME_DFC:
+      entries.push(
+        ...quantumDfcTokens.map((token) => ({
           label: token,
           data: history
             .filter(
@@ -179,9 +298,9 @@ export function toLineChartData(history: QuantumStats[], { type, timeFrame }: Li
         })),
       )
       break
-    case QuantumChartDataType.TOTAL_VOLUME_ETH:
+    case QuantumChartDataType.QUANTUM_TOTAL_VOLUME_ETH:
       entries.push(
-        ...ethereumTokens.map((token) => ({
+        ...quantumEthTokens.map((token) => ({
           label: token,
           data: history
             .filter(
